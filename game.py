0f1268b01cdfaadc930785774283728f91ade563
Kristhaly <christianoadams4400@gmail.com>
2024-12-07 22:57:08 -0500

    Updated game.py

--- game.py
+++ game.py
@@ -1,1 +1,22 @@
+import pygame
 
+pygame.init()
+
+window_width = 800
+window_height = 600
+
+window = pygame.display.set_mode((window_width, window_height))
+
+pygame.display.set_caption("Halium b/0.1")
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    window.fill((0, 0, 0))
+
+    pygame.display.update()
+
+pygame.quit()
